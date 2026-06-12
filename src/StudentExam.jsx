29e4909bf00f49fc420b162268b58c839374cd48
Luun0 <ekkozuparbaj@gmail.com>
2026-06-12 14:01:09 +0500

    Fix import typo for StudentChat component

--- src/StudentExam.jsx
+++ src/StudentExam.jsx
@@ -6,7 +6,7 @@ import { useTabGuard } from './useTabGuard';
 import { useExamTimer } from './useExamTimer';
 import { QUESTIONS } from './domain/examQuestions';
 import { submitAnswers } from './application/api/studentApi';
-import StudentChat from './Studentсhat';
+import StudentChat from './StudentChat';
 
 export default function StudentExam({ session, onLogout }) {
   const { studentId, username } = session;
